e44f54b150d726b130f67b63094f79e891e08df9
alexandrpryimak <s.pryimak@leadintelligence.biz>
2019-06-21 15:19:02 +0300

    markup first slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,8 @@
 // jquery code
 $(document).ready(function() {
     // toolbarTransform();
+    initThumbnailSlider();
+    // initModalThumbnailSlider();
     $(".country").select2({
         templateResult: formatState,
         templateSelection: formatState
@@ -93,6 +95,262 @@ function formatState (opt) {
         return $opt;
     }
 };
+
+function initMap() {
+    // The location of Uluru
+    var portDAndratx = {lat: 39.544, lng: 2.389};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 14.5, center: portDAndratx});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: portDAndratx, map: map});
+}
+
+function initModalThumbnailSlider() {
+    var sync3 = $('#sync3');
+
+    var sync4 = $('#sync4');
+    if(!(sync3.length && sync4.length)){
+        return
+    }
+    var sliderItem = sync4.children('div.item');
+
+    var slidesPerPage = 12; //globaly define number of elements per page
+    var syncedSecondary = false;
+
+    sync3
+        .owlCarousel({
+            items: 1,
+            slideSpeed: 2000,
+            nav: true,
+            autoplay: false,
+            dots: false,
+            loop: true,
+            responsiveRefreshRate: 200,
+            navText: [
+                '<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>',
+                '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>',
+            ],
+            responsive: {
+                767: {
+                    nav: false
+                }
+            }
+        })
+        .on('changed.owl.carousel', syncPosition);
+
+    sync4
+        .on('initialized.owl.carousel', function() {
+            sync4
+                .find('.owl-item')
+                .eq(0)
+                .addClass('current');
+        })
+        .owlCarousel({
+            items: slidesPerPage,
+            dots: false,
+            nav: true,
+            smartSpeed: 200,
+            slideSpeed: 500,
+            slideBy: slidesPerPage, //alternatively you can slide by 1, this way the active slide will stick to the first item in the second carousel
+            responsiveRefreshRate: 100,
+        })
+        .on('changed.owl.carousel', syncPosition4);
+
+    function syncPosition(el) {
+        //if you set loop to false, you have to restore this next line
+        //var current = el.item.index;
+
+        //if you disable loop you have to comment this block
+        var count = el.item.count - 1;
+        var current = Math.round(el.item.index - el.item.count / 2 - 0.5);
+
+        if (current < 0) {
+            current = count;
+        }
+        if (current > count) {
+            current = 0;
+        }
+
+        //end block
+
+        sync4
+            .find('.owl-item')
+            .removeClass('current')
+            .eq(current)
+            .addClass('current');
+        var onscreen = sync4.find('.owl-item.active').length - 1;
+        var start = sync4
+            .find('.owl-item.active')
+            .first()
+            .index();
+        var end = sync4
+            .find('.owl-item.active')
+            .last()
+            .index();
+
+        if (current > end) {
+            sync3.data('owl.carousel').to(current, 100, true);
+        }
+        if (current < start) {
+            sync3.data('owl.carousel').to(current - onscreen, 100, true);
+        }
+    }
+
+    function syncPosition4(el) {
+        if (syncedSecondary) {
+            var number = el.item.index;
+            sync3.data('owl.carousel').to(number, 100, true);
+        }
+    }
+
+    sync4.on('click', '.owl-item', function(e) {
+        e.preventDefault();
+        var number = $(this).index();
+        sync3.data('owl.carousel').to(number, 300, true);
+    });
+
+
+    function resizeThumbnails(){
+        $(window).resize(function() {
+            if ($(window).width() > 991) {
+                setItemHeight(sliderItem);
+            } else {
+                sliderItem.css({
+                    height: 'auto',
+                });
+            }
+        });
+        $(window).trigger('resize');
+    }
+    resizeThumbnails()
+}
+function initThumbnailSlider(){
+    var sync1 = $('#sync1');
+
+    var sync2 = $('#sync2');
+    if(!(sync1.length && sync2.length)){
+        return
+    }
+    var sliderItem = sync2.children('div.item');
+
+    var slidesPerPage = 12; //globaly define number of elements per page
+    var syncedSecondary = false;
+
+    sync1
+        .owlCarousel({
+            items: 1,
+            slideSpeed: 2000,
+            nav: true,
+            autoplay: false,
+            dots: false,
+            loop: true,
+            responsiveRefreshRate: 200,
+            navText: [
+                '<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>',
+                '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>',
+            ],
+            responsive: {
+                767: {
+                    nav: false
+                }
+            }
+        })
+        .on('changed.owl.carousel', syncPosition);
+
+    sync2
+        .on('initialized.owl.carousel', function() {
+            sync2
+                .find('.owl-item')
+                .eq(0)
+                .addClass('current');
+        })
+        .owlCarousel({
+            items: slidesPerPage,
+            dots: false,
+            nav: true,
+            smartSpeed: 200,
+            slideSpeed: 500,
+            slideBy: slidesPerPage, //alternatively you can slide by 1, this way the active slide will stick to the first item in the second carousel
+            responsiveRefreshRate: 100,
+        })
+        .on('changed.owl.carousel', syncPosition2);
+
+    function syncPosition(el) {
+        //if you set loop to false, you have to restore this next line
+        //var current = el.item.index;
+
+        //if you disable loop you have to comment this block
+        var count = el.item.count - 1;
+        var current = Math.round(el.item.index - el.item.count / 2 - 0.5);
+
+        if (current < 0) {
+            current = count;
+        }
+        if (current > count) {
+            current = 0;
+        }
+
+        //end block
+
+        sync2
+            .find('.owl-item')
+            .removeClass('current')
+            .eq(current)
+            .addClass('current');
+        var onscreen = sync2.find('.owl-item.active').length - 1;
+        var start = sync2
+            .find('.owl-item.active')
+            .first()
+            .index();
+        var end = sync2
+            .find('.owl-item.active')
+            .last()
+            .index();
+
+        if (current > end) {
+            sync2.data('owl.carousel').to(current, 100, true);
+        }
+        if (current < start) {
+            sync2.data('owl.carousel').to(current - onscreen, 100, true);
+        }
+    }
+
+    function syncPosition2(el) {
+        if (syncedSecondary) {
+            var number = el.item.index;
+            sync1.data('owl.carousel').to(number, 100, true);
+        }
+    }
+
+    sync2.on('click', '.owl-item', function(e) {
+        e.preventDefault();
+        var number = $(this).index();
+        sync1.data('owl.carousel').to(number, 300, true);
+    });
+
+
+    function resizeThumbnails(){
+        $(window).resize(function() {
+            if ($(window).width() > 991) {
+                setItemHeight(sliderItem);
+            } else {
+                sliderItem.css({
+                    height: 'auto',
+                });
+            }
+        });
+        $(window).trigger('resize');
+    }
+    resizeThumbnails()
+}
+
+
+function  setItemHeight(sliderItem) {
+    var itemWidth = sliderItem.width();
+    var itemHeight = itemWidth
+    sliderItem.css({height: itemHeight+'px'})
+}
 
 // function toolbarTransform(params) {
 //     var toolbarBg = $('.wrap-toolbar');
